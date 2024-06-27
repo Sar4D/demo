7a18be1c770c81bdd1abd0920d02976933677583
Sar4D <100158491+Sar4D@users.noreply.github.com>
2024-06-27 15:35:09 -0400

    use from plain text to force an alert in psscript analyzer

--- demo-pwsh.ps1
+++ demo-pwsh.ps1
@@ -1,1 +1,7 @@
 #this is a demo for code scanning
+
+#this is a demo for code scanning
+$unsecuredString = "password123"
+$secureString = ConvertTo-SecureString -String $unsecuredString -AsPlainText -Force
+
+Write-Output $secureString
